ec6af5d82ee2cee384dc9be64b86c31e7cdfa4e4
cheekyj <69768429+cheekyjmusic@users.noreply.github.com>
2023-10-01 14:47:48 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,8 +2,7 @@ var menuState = "closed";
 const menuContainer = document.querySelector('.menu-container');
 const menuShadow = document.querySelector('.menu-shadow');
 const menuButton = document.querySelector('.menu-button');
-
-
+const menuButtonImg = document.querySelector('.menu-button-img');
 
 function toggleMenu() {
     if (menuState === "closed") {
@@ -13,6 +12,7 @@ function toggleMenu() {
         menuShadow.style.opacity = 0.5;
         menuShadow.style.pointerEvents = 'auto';
         menuButton.style.position = 'fixed';
+        menuButtonImg.style.filter = 'invert(0%)';
     } else if (menuState === "open") {
         menuState = "closed";
         menuContainer.classList.remove("menu-offscreen");
@@ -20,6 +20,7 @@ function toggleMenu() {
         menuShadow.style.opacity = 0;
         menuShadow.style.pointerEvents = 'none';
         menuButton.style.position = 'absolute';
+        menuButtonImg.style.filter = 'invert(100%)';
 
     }
 }
