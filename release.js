d3b766a9d8dcf0365218467394e9858063a3d3af
cheekyj <69768429+cheekyjmusic@users.noreply.github.com>
2023-10-08 14:40:39 -0700

    updated

---
+++ release.js
@@ -0,0 +1,16 @@
+var targetDate = new Date('2023-10-13T17:00:00');
+
+        function checkUpcoming() {
+         var currentDate = new Date();
+         var upcoming = document.getElementById('upcoming');
+
+        if (currentDate <= targetDate) {
+            upcoming.style.display = 'none';
+            upcoming.style.pointerEvents = 'none';
+         } else {
+            upcoming.style.display = 'inline-block';
+            upcoming.style.pointerEvents = 'auto';
+         }
+        }
+
+setInterval(checkUpcoming, 1000);
